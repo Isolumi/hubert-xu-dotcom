328e48a01e013e27709e89bdc9c214d75f1abb77
Isolumi <hubertx98@gmail.com>
2026-02-20 20:23:30 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import fs from 'fs'
-import path from 'path'
+import asciiArt from '@/lib/ascii-art'
 import HomeClient from '@/components/HomeClient'
 
 export default function Home() {
-  const asciiArt = fs.readFileSync(path.join(process.cwd(), 'public/ascii-art.txt'), 'utf-8')
-
   return <HomeClient asciiArt={asciiArt} />
 }
